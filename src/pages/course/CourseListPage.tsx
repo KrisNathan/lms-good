a7e70281ec774a4a142758d22706a1339dae0ce7
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-05-19 18:07:55 +0700

    feat: functioning link to course page

---
+++ src/pages/course/CourseListPage.tsx
@@ -0,0 +1,34 @@
+import HorizontalTab from "../../components/tab/HorizontalTab";
+import ClassCard from "../../components/ClassCard";
+
+export default function CourseListPage() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="mx-auto">
+        <HorizontalTab tabs={["All", "CL", "LAB", "LEC"]} />
+      </div>
+      <div className="mx-32 flex flex-col gap-5">
+        <ClassCard
+
+          courseCode="AIUEO"
+          courseName="Data Structures"
+          instructor="Skibidi Philip, M.Kom."
+          onClick={() => {
+            console.log("hi");
+            window.location.href = "/course/course123";
+          }}
+        />
+        <ClassCard
+          courseCode="AIUEO"
+          courseName="Data Structures"
+          instructor="Skibidi Philip, M.Kom."
+        />
+        <ClassCard
+          courseCode="AIUEO"
+          courseName="Data Structures"
+          instructor="Skibidi Philip, M.Kom."
+        />
+      </div>
+    </div>
+  )
+}
