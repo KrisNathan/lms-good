d4e6bb0986c4a0b1f0c4a8c36da5cf5391320a79
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-05-19 18:00:52 +0700

    feat: CardSideButton

---
+++ src/components/buttons/CardSideButton.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  icon: string;
+  text: string;
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+export default function CardSideButton({ icon, text, className = '', onClick = (e) => { } }: Props) {
+  return <button className={`flex flex-row w-fit gap-1 rounded-r-2xl px-5 items-center ${className}`}
+    onClick={onClick}>
+    <img src={icon}></img>
+    <div>{text}</div>
+  </button>
+}
