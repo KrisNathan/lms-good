262686922ecd3828292349fa4c07d9e4174df2cc
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-05-19 18:00:44 +0700

    feat: optional className

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,11 +1,11 @@
 interface Props {
   icon: string;
   text: string;
-  className: string;
+  className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ icon, text, className, onClick = (e) => { } }: Props) {
+export default function Button({ icon, text, className = '', onClick = (e) => { } }: Props) {
   return <button className={`flex flex-row w-fit gap-1 rounded-2xl py-1 px-5 ${className}`}
     onClick={onClick}>
     <img src={icon}></img>
