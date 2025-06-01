c7b7f1938ccf6538d258132fdef07f45e2ebcfa8
Jeremy <jer.auriel@gmail.com>
2025-06-01 22:15:51 +0700

    feat: attendance button is now clickable in attendcard

--- src/components/course_session/AttendCard.tsx
+++ src/components/course_session/AttendCard.tsx
@@ -1,16 +1,25 @@
 import CardSideButton from "../buttons/CardSideButton";
-
+import { useState } from "react";
 interface Props {
 
 }
 
 export default function AttendCard() {
+  const [attended, setAttended] = useState(false);
+
   return (
-    <div className="flex flex-row border-solid border-1 border-slate-200 rounded-2xl">
+    <div className="flex flex-row border border-slate-200 rounded-2xl">
       <div className="p-5">
         <div>Attendance</div>
       </div>
-      <CardSideButton text="Attend" icon="" className="bg-red-200 hover:bg-red-300 ml-auto" />
+      <CardSideButton
+        text={attended ? "Attended" : "Attend"}
+        icon=""
+        className={`ml-auto transition-colors ${
+          attended ? "bg-green-300" : "bg-red-200 hover:bg-red-300"
+        }`}
+        onClick={() => setAttended(true)}
+      />
     </div>
   );
 }
