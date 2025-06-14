5f63a7c28f5df89d7b2c5afd95b6de0f2dd6e7ad
KrisNathan <kristophernathanael2005@gmail.com>
2025-06-14 18:35:57 +0700

    style: learning outcomes boxed with procedural listing

--- src/components/course_session/SyllabusTab.tsx
+++ src/components/course_session/SyllabusTab.tsx
@@ -1,20 +1,29 @@
+const syllabuses: string[] = [
+  "LO1: (C2) Comprehension : describe the concept of interaction design",
+  "LO2: (C3) Application : use guidelines, principles, models, and framework related with interaction design",
+  "LO3: (C6) Evaluation : choose the data gathering technique from user to develop successful interaction design",
+  "LO4: (C3) Application : design the user requirements with interaction styles",
+  "LO5: (C6) Evaluation : evaluate the user interfaces of interactive software"
+];
+
 export default function SyllabusTab() {
   return <>
     <div>This course comprises the human factors in interactive software, theories, principles and guidance in interface development, interface components, interface styles, disciplines associated with design and evaluation of user interface in order to support the usability. This course gives students the ability to design and evaluate the user interface.</div>
-    <div>Learning Outcomes</div>
-    <div>
-      On successful completion of this course, student will be able to:
-    </div>
-    <ol>
-      <li>
-        LO1: (C2) Comprehension : describe the concept of interaction design
-      </li>
-    </ol>
-    <div>
-      LO2: (C3) Application : use guidelines, principles, models, and framework related with interaction design
-      LO3: (C6) Evaluation : choose the data gathering technique from user to develop successful interaction design
-      LO4: (C3) Application : design the user requirements with interaction styles
-      LO5: (C6) Evaluation : evaluate the user interfaces of interactive software
+    <div className="flex flex-col gap-2 rounded-2xl border-solid border-1 border-slate-200 p-5">
+      <div>
+        <div className="font-semibold">Learning Outcomes</div>
+        <div>
+          On successful completion of this course, student will be able to:
+        </div>
+      </div>
+      <ol className="flex flex-col gap-1">
+        {syllabuses.map((syl) => (
+          <li>
+            {syl}
+          </li>
+        ))}
+      </ol>
+
     </div>
   </>
 }
