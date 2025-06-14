2503a1f6d3f625e5e98e994991cec92556a4bcf4
KrisNathan <kristophernathanael2005@gmail.com>
2025-06-14 18:10:43 +0700

    fix: course list page responsive

--- src/pages/course/CourseListPage.tsx
+++ src/pages/course/CourseListPage.tsx
@@ -10,54 +10,57 @@ export default function CourseListPage() {
   };
 
   return (
-    <div className="flex flex-col gap-5">
-       <button
-          onClick={handleBackClick}
-          className="mb-4 ml-31 flex items-center text-gray-600 hover:text-gray-900 transition-colors"
+    <div className="pt-6 flex flex-col gap-5">
+      <button
+        onClick={handleBackClick}
+        className="flex items-center text-gray-600 hover:text-gray-900 transition-colors"
+      >
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
         >
-          <svg 
-            className="w-5 h-5 mr-2" 
-            fill="none" 
-            stroke="currentColor" 
-            viewBox="0 0 24 24"
-          >
-            <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              strokeWidth={2} 
-              d="M15 19l-7-7 7-7" 
-            />
-          </svg>
-          Back to Home
-        </button>
-      <div className="mx-auto">
-        <HorizontalTab activeTab={activeTab} setActiveTab={setActiveTab} tabs={["All", "CL", "LAB", "LEC"]} />
-      </div>
-      <div className="mx-32 flex flex-col gap-5">
-        <ClassCard
-          courseCode="AIUEO"
-          courseName="Data Structures"
-          instructor="Skibidi Philip, M.Kom."
-          onClick={() => {
-            window.location.href = "/course/course123";
-          }}
-        />
-        <ClassCard
-          courseCode="AIUEO"
-          courseName="Data Structures"
-          instructor="Skibidi Philip, M.Kom."
-          onClick={() => {
-            window.location.href = "/course/course123";
-          }}
-        />
-        <ClassCard
-          courseCode="AIUEO"
-          courseName="Data Structures"
-          instructor="Skibidi Philip, M.Kom."
-          onClick={() => {
-            window.location.href = "/course/course123";
-          }}
-        />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+        Back to Home
+      </button>
+      <div className="flex flex-col gap-5 items-center">
+        <div className="mx-auto">
+          <HorizontalTab activeTab={activeTab} setActiveTab={setActiveTab} tabs={["All", "CL", "LAB", "LEC"]} />
+        </div>
+        <div className="md:min-w-xl sm:w-fit flex flex-col gap-5 items-center">
+          <ClassCard
+            courseCode="AIUEO"
+            courseName="Data Structures"
+            instructor="Skibidi Philip, M.Kom."
+            onClick={() => {
+              window.location.href = "/course/course123";
+            }}
+          />
+          <ClassCard
+            courseCode="AIUEO"
+            courseName="Data Structures"
+            instructor="Skibidi Philip, M.Kom."
+            onClick={() => {
+              window.location.href = "/course/course123";
+            }}
+          />
+          <ClassCard
+            courseCode="AIUEO"
+            courseName="Data Structures"
+            instructor="Skibidi Philip, M.Kom."
+            onClick={() => {
+              window.location.href = "/course/course123";
+            }}
+          />
+        </div>
+
       </div>
     </div>
   )
