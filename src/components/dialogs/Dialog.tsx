cf5a599bf74f2451d160bcfa0d9f6a688fe2bbb9
KrisNathan <kristophernathanael2005@gmail.com>
2025-06-17 10:22:41 +0700

    fix: Dialog content nonscrollable if it overflows
    Solved by adding overflow-y-auto to the content div.
    
    Fixes #17

--- src/components/dialogs/Dialog.tsx
+++ src/components/dialogs/Dialog.tsx
@@ -45,7 +45,7 @@ export default function Dialog({
         </div>
 
         {/* Content */}
-        <div className="flex-1 overflow-hidden">
+        <div className="flex-1 overflow-y-auto">
           {children}
         </div>
       </div>
