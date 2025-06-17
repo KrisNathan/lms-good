69d068fd4dd98e50d3619f9497b1eced4338a3ef
KrisNathan <kristophernathanael2005@gmail.com>
2025-06-17 11:14:35 +0700

    style: layout restyling with rounded corners and less intrusive shadows

--- src/components/NavSideBar.tsx
+++ src/components/NavSideBar.tsx
@@ -27,11 +27,41 @@ export default function NavSidebar({ children }: { children: React.ReactNode }) 
   ];
 
   return (
-    <div className="flex font-sans bg-gray-50 text-gray-900 min-h-screen">
+    <div className="flex font-sans text-gray-900 min-h-screen h-full">
+      <div className="flex-1 flex flex-col w-full h-full">
+
+        {/* Navbar */}
+        <header className="flex justify-between items-center h-16 px-5 bg-white fixed top-0 left-0 right-0">
+          <div className="flex items-center gap-4">
+            <button onClick={toggleSidebar} className="hidden md:flex flex-col hover:bg-slate-100 rounded-full overflow-hidden p-2  hover:scale-105 active:scale-95 cursor-pointer">
+              <Menu size={24} className={`text-gray-600 transition-transform ${sidebarExpanded ? 'rotate-90' : ''}`} />
+            </button>
+            <a href="/home" className="text-2xl font-bold text-gray-900">SUNIB</a>
+          </div>
+          <div className="flex items-center gap-6">
+            <a href="/faq"><HelpCircle size={28} /></a>
+            <div className="w-8 h-8 rounded-full bg-gray-200 flex justify-center items-center">
+              <UserCircle size={28} />
+            </div>
+          </div>
+        </header>
+
+        {/* Main Content */}
+        <main
+          className={`py-20 md:pb-0 transition-all duration-300 flex flex-row h-full`}
+        >
+          <div className={`transition-all duration-300 h-full ${sidebarExpanded ? 'md:w-60' : 'md:w-20'}`}></div>
+          <div className="flex-1 min-w-0 px-9 bg-gray-50 md:rounded-2xl overflow-y-auto h-full shadow-sm z-40">
+            {children}
+          </div>
+        </main>
+        <div className="h-18 md:h-0"></div>
+      </div>
+
 
       {/* Fixed Sidebar */}
       <aside
-        className={`hidden md:block fixed top-16 left-0 z-40 h-[calc(100vh-4rem)] bg-white border-r border-gray-200 shadow-sm transition-all duration-300
+        className={`hidden md:block fixed top-16 left-0 h-[calc(100vh-4rem)] bg-white transition-all duration-300
           ${sidebarExpanded ? "w-60" : "w-20"}`}
       >
         <nav className="py-2 px-2">
@@ -61,38 +91,8 @@ export default function NavSidebar({ children }: { children: React.ReactNode }) 
         </nav>
       </aside>
 
-      <div className="flex-1 flex flex-col w-full">
-
-        {/* Navbar */}
-        <header className="flex justify-between items-center h-16 px-5 bg-white border-b border-gray-200 shadow-sm fixed top-0 left-0 right-0 z-50">
-          <div className="flex items-center gap-4">
-            <button onClick={toggleSidebar} className="hidden md:flex flex-col hover:bg-slate-100 rounded-full overflow-hidden p-2  hover:scale-105 active:scale-95 cursor-pointer">
-              <Menu size={24} className={`text-gray-600 transition-transform ${sidebarExpanded ? 'rotate-90' : ''}`} />
-            </button>
-            <a href="/home" className="text-2xl font-bold text-gray-900">SUNIB</a>
-          </div>
-          <div className="flex items-center gap-6">
-            <a href="/faq"><HelpCircle size={28} /></a>
-            <div className="w-8 h-8 rounded-full bg-gray-200 flex justify-center items-center">
-              <UserCircle size={28} />
-            </div>
-          </div>
-        </header>
-        
-        {/* Main Content */}
-        <main
-          className={`py-20 transition-all duration-300 flex flex-row`}
-        >
-          <div className={`transition-all duration-300 ${sidebarExpanded ? 'md:w-60' : 'md:w-20'}`}></div>
-          <div className="flex-1 min-w-0 px-9">
-            {children}
-          </div>
-        </main>
-        <div className="h-18 md:h-0"></div>
-      </div>
-
       {/* Bottom Nav for Mobile */}
-      <nav className="fixed bottom-0 w-full bg-white border-t border-gray-200 shadow-md md:hidden">
+      <nav className="fixed bottom-0 w-full bg-white md:hidden">
         <ul className="flex justify-around py-2">
           {navItems.map((item) => {
             const isActive = currentPath.startsWith(item.path);
